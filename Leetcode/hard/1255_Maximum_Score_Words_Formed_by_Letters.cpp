72ef558b5703c47682847ef32e319313985a9e7d
toantang <toantangk34alnq@gmail.com>
2022-09-23 17:16:12 +0700

    change gitignore file

--- Leetcode/hard/1255_Maximum_Score_Words_Formed_by_Letters.cpp
+++ Leetcode/hard/1255_Maximum_Score_Words_Formed_by_Letters.cpp
@@ -2,146 +2,166 @@
 
 using namespace std;
 
-static const int MAX_ALPHABET = 123;
-int flag[MAX_ALPHABET];
-int sign[15];
-int scoreWords[15];
-int maxScore = 0;
+struct Character {
+    int quantity;
+    int score;
+    char label;
+    Character create(char label, int quantity, int score) {
+        Character c;
+        c.quantity = quantity;
+        c.score = score;
+        c.label = label;
+        return c;
+    }
+    string toString() {
+        string s = "";
+        if (label != '~') s.push_back(label);
+        return s + "(" + to_string(quantity) + ", " + to_string(score) + ")";
+    }
+    int totalScore() {
+        return quantity * score;
+    }
+};
+
+vector<Character> characters = {};
 int ans = 0;
+int f = 0;
+int flag[15];
+int scoreWords[15];
+int totalRemaining = 0;
+int totalScoreWords = 0;
+int cnt = 0;
 
-void createSign(int n) {
-    for (int i = 0; i < n; i++) {
-        sign[i] = -1;
+void createCharacter(vector<string>& words, vector<char>& letters, vector<int>& score) {
+    for (int i = 0; i < 27; i++) {
+        Character c;
+        c = c.create('~', 0, 0);
+        characters.push_back(c);
     }
-}
+    for (int i = 0; i < letters.size(); i++) {
+        characters[letters[i] - 'a'].label = letters[i];
+        characters[letters[i] - 'a'].quantity++;
+    }
 
-void createFlag(vector<char>& letters) {
-    int len = letters.size();
-    for (int i = 0; i < len; i++) {
-        flag[i] = 0;
+    for (int i = 0; i < characters.size(); i++) {
+        characters[i].score = score[i];
     }
-    for (int i = 0; i < len; i++) {
-        int x = letters[i];
-        flag[x] = flag[x] + 1;
-    }
-    cout << "flag: ";
-    for (int i = 97; i < MAX_ALPHABET; i++) {
-        cout << flag[i] << ", ";
-    }
-    cout << endl;
-}
 
-void createScoreWords(vector<string>& words, vector<int>& score) {
-    int len = words.size();
-    for (int i = 0; i < len; i++) {
-        for (int j = 0; j < words[i].size(); j++) {
-            char x = words[i][j];
-            scoreWords[i] += score[x - 97];
-        }
-    }
-    cout << "score words: ";
-    for (int i = 0; i < len; i++) {
-        cout << scoreWords[i] << ", ";
-    }
-    cout << endl;
-}
-
-void decreaseChar(string word) {
-    for (int i = 0; i < word.size(); i++) {
-        flag[word[i]]--;
-    }
-    // cout << "flag: ";
-    // for (int i = 97; i < MAX_ALPHABET; i++) {
-    //     cout << flag[i] << ", ";
+    // for (int i = 0; i < characters.size(); i++) {
+    //     cout << characters[i].toString() << ", ";
     // }
     // cout << endl;
 }
 
-void increaseChar(string word) {
+void createFlag(int n) {
+    for (int i = 0; i < n; i++) {
+        flag[i] = -1;
+    }
+}
+
+void createScoreWords(vector<string>& words, vector<char>& letters, vector<int>& score) {
+    for (int i = 0; i < words.size(); i++) {
+        string word = words[i];
+        for (int j = 0; j < word.size(); j++) {
+            scoreWords[i] += characters[word[j] - 'a'].score;
+        }
+    }
+    for (int i = 0; i < words.size(); i++) {
+        cout << scoreWords[i] << ", ";
+        totalScoreWords += scoreWords[i];
+    }
+    cout << endl;
+}
+
+void calTotalRemaining() {
+    for (int i = 0; i < characters.size(); i++) {
+        totalRemaining += characters[i].totalScore();
+    }
+}
+
+void decrease(string word) {
     for (int i = 0; i < word.size(); i++) {
-        flag[word[i]]++;
+        //Character c = characters[word[i] - 'a'];
+        characters[word[i] - 'a'].quantity--;
+        totalRemaining -= characters[word[i] - 'a'].score;
+    }
+}
+
+void increase(string word) {
+    for (int i = 0; i < word.size(); i++) {
+        //Character c = characters[word[i] - 'a'];
+        characters[word[i] - 'a'].quantity++;
+        totalRemaining += characters[word[i] - 'a'].score;
     }
 }
 
 bool isCanAdd(string word) {
-    int temp[MAX_ALPHABET];
-    for (int i = 97; i < MAX_ALPHABET; i++) {
-        temp[i] = 0;
+    vector<Character> temp = {};
+    for (int i = 0; i < 27; i++) {
+        Character c;
+        c = c.create('~', 0, 0);
+        temp.push_back(c);
+    }
+    for (int i = 0; i < word.size(); i++) {
+        Character c;
+        temp[word[i] - 'a'].label = word[i];
+        temp[word[i] - 'a'].quantity++;
     }
     
-    cout << endl;
     for (int i = 0; i < word.size(); i++) {
-        int index = word[i];
-        temp[index] = temp[index] + 1;
+        Character c = characters[word[i] - 'a'];
+        if (c.quantity <= 0 || c.quantity < temp[word[i] - 'a'].quantity) return false;
     }
 
-    // cout << "temp: ";
-    // for (int i = 97; i < MAX_ALPHABET; i++) {
-    //     cout << temp[i] << ", ";
-    // }
-    for (int i = 97; i < MAX_ALPHABET; i++) {
-        if (temp[i] > flag[i]) return false;
-    }
-    
     return true;
 }
 
-bool isCanGo(vector<char>& letters) {
-    for (int i = 97; i < MAX_ALPHABET; i++) {
-        
-    }
-}
-void TRY(int i, int n, vector<string>& words) {
-    maxScore = maxScore + scoreWords[i];
-    decreaseChar(words[i]);
-    sign[i] = 1;
-    cout << "flag: ";
-    for (int i = 97; i < MAX_ALPHABET; i++) {
-        cout << flag[i] << ", ";
-    }
-    cout << "maxScore: " << maxScore << endl;
-    if (maxScore > ans) {
-        ans = maxScore;
-        //cout << "ans: " << ans << endl;
+void TRY(int i, int n, vector<string>& words, vector<char>& letters, vector<int>& score) {
+    f = f + scoreWords[i];
+    totalScoreWords -= scoreWords[i];
+    flag[i] = 1;
+    decrease(words[i]);
+
+    cout << "f: " << f << ", totalRemaining: " << totalRemaining << endl;
+    cnt++;
+    if (f > ans) {
+        ans = f;
     }
 
     for (int j = 0; j < n; j++) {
-        bool isCan = isCanAdd(words[j]);
-        
-        if (sign[j] < 0 && isCan) {
-            TRY(j, n, words);
+        if (flag[j] < 0 && isCanAdd(words[j]) && f + totalScoreWords > ans) {
+            TRY(j, n, words, letters, score);
         }
     }
-    
-    sign[i] = -1;
-    increaseChar(words[i]);
-    maxScore = maxScore - scoreWords[i];
-}
 
-int maxScoreWords(vector<string>& words, vector<char>& letters, vector<int>& score) {
-    return 0;
+    increase(words[i]);
+    f = f - scoreWords[i];
+    totalScoreWords += scoreWords[i];
+    flag[i] = -1;
 }
 
 int main() {
     vector<string> words;
     vector<char> letters;
     vector<int> score;
-    //words = {"dog","cat","dad","good"}, letters = {'a','a','c','d','d','d','g','o','o'}, score = {1,0,9,5,0,0,3,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0};
-    //words = {"leetcode"}, letters = {"l","e","t","c","o","d"}, score = {0,0,1,1,1,0,0,0,0,0,0,1,0,0,1,0,0,0,0,1,0,0,0,0,0,0};
+    // words = {"dog","cat","dad","good"}, letters = {'a','a','c','d','d','d','g','o','o'}, score = {1,0,9,5,0,0,3,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0};
+    //words = {"leetcode"}, letters = {'l','e','t','c','o','d'}, score = {0,0,1,1,1,0,0,0,0,0,0,1,0,0,1,0,0,0,0,1,0,0,0,0,0,0};
     words = { "daeagfh","acchggghfg","feggd","fhdch","dbgadcchfg","b","db","fgchfe","baaedddc"}, letters = { 'a','a','a','a','a','a','a','b','b','b','b',
     'b','b','b','b','b','c','c','c','c','c','c','c','c','c','c','c','d','d','d','d','d','d','d','d','d','d','d','d','d','d','e','e','e','e','e','e','e',
     'e','e','e','f','f','f','f','f','f','f','f','f','f','f','f','f','f','g','g','g','g','g','g','g','g','g','g','g','g','h','h','h','h','h','h','h','h',
     'h','h','h','h','h'}, score = { 2,1,9,2,10,5,7,8,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+    //words = {"xxxz","ax","bx","cx"}, letters = {'z','a','b','c','x','x','x'}, score = {4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,5,0,10};
     int n = words.size();
-    createFlag(letters);
-    createScoreWords(words, score);
-    createSign(n);
+    createCharacter(words, letters, score);
+    createFlag(n);
+    createScoreWords(words, letters, score);
+    calTotalRemaining();
+    cout << "total remaining: " << totalRemaining << endl;
     for (int i = 0; i < n; i++) {
-        if (sign[i] < 0 && isCanAdd(words[i])) {
-            TRY(i, n, words);
+        if (flag[i] < 0 && isCanAdd(words[i])) {
+            TRY(i, n, words, letters, score);
         }
     }
-    
-    cout << "ans: " << ans << endl;
-    return 0;
+    cout << "cnt: " << cnt << endl;
+    cout << "ans: " << ans << endl; 
 }
