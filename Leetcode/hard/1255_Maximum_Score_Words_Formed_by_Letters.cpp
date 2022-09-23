cc550e78e4a41765e0ea16e6b26f84a581f25d86
toantang <toantangk34alnq@gmail.com>
2022-09-23 09:34:33 +0700

    continu to code 1255

--- Leetcode/hard/1255_Maximum_Score_Words_Formed_by_Letters.cpp
+++ Leetcode/hard/1255_Maximum_Score_Words_Formed_by_Letters.cpp
@@ -15,13 +15,13 @@ void createSign(int n) {
     }
 }
 
-void createFlag(vector<string>& letters) {
+void createFlag(vector<char>& letters) {
     int len = letters.size();
     for (int i = 0; i < len; i++) {
         flag[i] = 0;
     }
     for (int i = 0; i < len; i++) {
-        int x = letters[i][0];
+        int x = letters[i];
         flag[x] = flag[x] + 1;
     }
     cout << "flag: ";
@@ -50,11 +50,11 @@ void decreaseChar(string word) {
     for (int i = 0; i < word.size(); i++) {
         flag[word[i]]--;
     }
-    cout << "flag: ";
-    for (int i = 97; i < MAX_ALPHABET; i++) {
-        cout << flag[i] << ", ";
-    }
-    cout << endl;
+    // cout << "flag: ";
+    // for (int i = 97; i < MAX_ALPHABET; i++) {
+    //     cout << flag[i] << ", ";
+    // }
+    // cout << endl;
 }
 
 void increaseChar(string word) {
@@ -64,30 +64,51 @@ void increaseChar(string word) {
 }
 
 bool isCanAdd(string word) {
-    for (int i = 0; i < word.size(); i++) {
-        // cout << "word[" << i << "] = " << word[i] << endl;
-        // cout << "flag[" << word[i] << "] = " << flag[i] << endl;
-        int index = word[i];
-        //cout << "index: " << index << ", flag: " << flag[index] << endl;
-        if (flag[index] <= 0) return false;
+    int temp[MAX_ALPHABET];
+    for (int i = 97; i < MAX_ALPHABET; i++) {
+        temp[i] = 0;
     }
+    
+    cout << endl;
+    for (int i = 0; i < word.size(); i++) {
+        int index = word[i];
+        temp[index] = temp[index] + 1;
+    }
+
+    // cout << "temp: ";
+    // for (int i = 97; i < MAX_ALPHABET; i++) {
+    //     cout << temp[i] << ", ";
+    // }
+    for (int i = 97; i < MAX_ALPHABET; i++) {
+        if (temp[i] > flag[i]) return false;
+    }
+    
     return true;
 }
 
+bool isCanGo(vector<char>& letters) {
+    for (int i = 97; i < MAX_ALPHABET; i++) {
+        
+    }
+}
 void TRY(int i, int n, vector<string>& words) {
     maxScore = maxScore + scoreWords[i];
     decreaseChar(words[i]);
-    cout << "maxScore: " << maxScore << endl;
     sign[i] = 1;
+    cout << "flag: ";
+    for (int i = 97; i < MAX_ALPHABET; i++) {
+        cout << flag[i] << ", ";
+    }
+    cout << "maxScore: " << maxScore << endl;
     if (maxScore > ans) {
         ans = maxScore;
-        cout << "ans: " << ans << endl;
+        //cout << "ans: " << ans << endl;
     }
 
     for (int j = 0; j < n; j++) {
         bool isCan = isCanAdd(words[j]);
+        
         if (sign[j] < 0 && isCan) {
-            cout << "nhay vao try" << endl;
             TRY(j, n, words);
         }
     }
@@ -103,13 +124,24 @@ int maxScoreWords(vector<string>& words, vector<char>& letters, vector<int>& sco
 
 int main() {
     vector<string> words;
-    vector<string> letters;
+    vector<char> letters;
     vector<int> score;
-    words = {"dog","cat","dad","good"}, letters = {"a","a","c","d","d","d","g","o","o"}, score = {1,0,9,5,0,0,3,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0};
+    //words = {"dog","cat","dad","good"}, letters = {'a','a','c','d','d','d','g','o','o'}, score = {1,0,9,5,0,0,3,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0};
+    //words = {"leetcode"}, letters = {"l","e","t","c","o","d"}, score = {0,0,1,1,1,0,0,0,0,0,0,1,0,0,1,0,0,0,0,1,0,0,0,0,0,0};
+    words = { "daeagfh","acchggghfg","feggd","fhdch","dbgadcchfg","b","db","fgchfe","baaedddc"}, letters = { 'a','a','a','a','a','a','a','b','b','b','b',
+    'b','b','b','b','b','c','c','c','c','c','c','c','c','c','c','c','d','d','d','d','d','d','d','d','d','d','d','d','d','d','e','e','e','e','e','e','e',
+    'e','e','e','f','f','f','f','f','f','f','f','f','f','f','f','f','f','g','g','g','g','g','g','g','g','g','g','g','g','h','h','h','h','h','h','h','h',
+    'h','h','h','h','h'}, score = { 2,1,9,2,10,5,7,8,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
     int n = words.size();
     createFlag(letters);
     createScoreWords(words, score);
     createSign(n);
-    TRY(0, n, words);
+    for (int i = 0; i < n; i++) {
+        if (sign[i] < 0 && isCanAdd(words[i])) {
+            TRY(i, n, words);
+        }
+    }
+    
+    cout << "ans: " << ans << endl;
     return 0;
 }
